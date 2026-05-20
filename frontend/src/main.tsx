9970f1c3d252a33a68c17f24685d8495adb3a3cf
narrowings <rafael.plazas@uao.edu.co>
2026-05-20 15:25:18 -0500

    correcciones front para deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
-import { setupMockApi } from './services/mockApi';
-import { http } from './services/api'; // ← importar la instancia
+
 
 // ⚠️ MODO DEMO: eliminar cuando el backend esté disponible
 //setupMockApi(http); // ← pasarle la instancia directamente
